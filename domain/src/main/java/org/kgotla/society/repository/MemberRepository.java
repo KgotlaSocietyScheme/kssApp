72875472c05ae7b202a471e4775a38903c18fc24
Mohapi27 <cassino.happies@gmail.com>
2019-09-30 23:23:56 +0200

    Repo added

---
+++ domain/src/main/java/org/kgotla/society/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package org.kgotla.society.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MemberRepository extends CrudRepository {
+}
